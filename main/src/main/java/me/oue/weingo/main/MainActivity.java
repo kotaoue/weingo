6ac420bffd6621e311f51819ac42a6a29334ab8a
kota oue <kotaoue@gmail.com>
2014-08-13 23:02:27 +0900

    [kotaoue]タブの色変更したりした。
    なんか、収まり悪いので、テキストカラーとサブカラーを分けて統一したい。
    BGとタブは同じ色にするとかかなぁ

--- main/src/main/java/me/oue/weingo/main/MainActivity.java
+++ main/src/main/java/me/oue/weingo/main/MainActivity.java
@@ -8,10 +8,12 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.TabHost;
+import android.widget.TextView;
 
-public class OuterFrameWidget extends TabActivity {
+public class MainActivity extends TabActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -34,14 +36,24 @@ public class OuterFrameWidget extends TabActivity {
         // 体重を記録するタブの準備
         intent = new Intent().setClass(this, RecordInputActivity.class);
         spec = tabHost.newTabSpec("input_tab");
-        spec.setIndicator(res.getString(R.string.input_tag_title), res.getDrawable(R.drawable.ic_tab_artists));
+
+        // タブ用のViewを作成。
+        View before_tab_view = View.inflate(getApplication(), R.layout.before_tab_view, null);
+        TextView before_tab_text = (TextView) before_tab_view.findViewById(R.id.TextViewTabTitle);
+        before_tab_text.setText(res.getString(R.string.input_tag_title));
+        spec.setIndicator(before_tab_view);
         spec.setContent(intent);
         tabHost.addTab(spec);
 
         // グラフを表示するタブの準備
         intent = new Intent().setClass(this, RecordViewActivity.class);
         spec = tabHost.newTabSpec("view_tab");
-        spec.setIndicator(res.getString(R.string.view_tag_title), res.getDrawable(R.drawable.ic_tab_artists));
+        // タブ用のViewを作成。
+        View after_tab_view = View.inflate(getApplication(), R.layout.after_tab_view, null);
+        TextView after_tab_text = (TextView) after_tab_view.findViewById(R.id.TextViewTabTitle);
+        after_tab_text.setText(res.getString(R.string.view_tag_title));
+        spec.setIndicator(after_tab_view);
+        // spec.setIndicator(res.getString(R.string.view_tag_title));
         spec.setContent(intent);
         tabHost.addTab(spec);
 
--- main/src/main/java/me/oue/weingo/main/RecordInputActivity.java
+++ main/src/main/java/me/oue/weingo/main/RecordInputActivity.java
@@ -2,11 +2,19 @@ package me.oue.weingo.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RecordInputActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.record_input);
+
+        Button buttonBefore = (Button) findViewById(R.id.buttonBefore);
+        buttonBefore.setTextColor(R.color.after_text);
+        // buttonBefore.setOnClickListener(this);
+
+        // TextView textview1=(TextView)findViewById(R.id.textview1);
+        // textview1.setTextColor(Color.RED);
     }
 }
